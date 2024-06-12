2050d9a3198449a46a5bb27c08dcb208b515b29f
karthikpohane <karthik.pohane@mca.christuniversity.in>
2024-06-12 16:14:39 +0530

    Update

---
+++ getToken.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const fs = require('fs');
+const { updateToken } = require('./refreshToken');
+
+async function getToken() {
+    let token;
+    try {
+        token = require('./token.json');
+        if (JSON.stringify(token).length < 20) {
+            token = false;
+        } else {
+            const tokenPayload = JSON.parse(Buffer.from(token.token.split('.')[1], 'base64').toString());
+            if (tokenPayload.exp * 1000 > Date.now()) {
+                //console.log("Token reused");
+                return [token.token, false];
+            } else {
+                token = false;
+            }
+        }
+    } catch (e) {
+        token = false;
+    }
+
+    if (!token) {
+        //console.log("Creating new token");
+        token = await login();
+        if (!token) return false;
+        return [token, true];
+    }
+}
+
+async function login() {
+    try {
+        const options = {
+            method: 'POST',
+            url: 'https://idp.app-framework.meltwater.io/login',
+            headers: {'Content-Type': 'application/json'},
+            data: {email: process.env.EMAIL, password: process.env.PASSWORD, rememberMe: true}
+        };
+
+        const result = await axios.request(options);
+        if (!result.data.success) return false;
+        updateToken(result.data.token)
+        return result.data.token;
+    } catch (e) {
+        console.log("Failed to generate token");
+        return false;
+    }
+}
+
+exports.getToken = getToken;
---
+++ refreshToken.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+async function updateToken(newToken) {
+    try {
+        fs.writeFile("token.json", JSON.stringify({ token: newToken }), err => {
+            if (err) {
+                return console.log("Token not updated");
+            }
+            console.log({ Message: "Token updated" });
+        });
+    } catch (err) {
+        console.error("Error in updating token file", err);
+    }
+}
+
+exports.updateToken = updateToken;
